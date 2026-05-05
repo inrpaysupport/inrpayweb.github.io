c705dd1d43118c1a126588e2059e00c8981f9ea0
neonxtech <neonxtech09@gmail.com>
2026-05-06 02:15:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
-import { getFirestore, doc, setDoc, getDoc, getDocs, updateDoc, collection } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-firestore.js";
+import { getFirestore, doc, setDoc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-firestore.js";
 
 const app = initializeApp({
 apiKey:"AIzaSyBh-J9LAYeCfxNoKw9C94gbCqVhELofuoo",
@@ -10,33 +10,9 @@ projectId:"inrpay-44413"
 const db=getFirestore(app);
 const get=id=>document.getElementById(id);
 
-function showMsg(t){
-get("msgText").innerText=t;
-get("msgBox").classList.add("active");
-}
-window.closeMsg=()=>get("msgBox").classList.remove("active");
-
-window.togglePass=()=>{
-let p=get("password");
-p.type=p.type==="password"?"text":"password";
-};
-
-window.onload=()=>showRegister();
-
-window.showRegister=()=>{
-get("authTitle").innerText="Create Account";
-get("name").style.display="block";
-get("registerBtn").style.display="block";
-get("loginBtn").style.display="none";
-get("forgotText").style.display="none";
-};
-
-window.showLogin=()=>{
-get("authTitle").innerText="Sign In";
-get("name").style.display="none";
-get("registerBtn").style.display="none";
-get("loginBtn").style.display="block";
-get("forgotText").style.display="block";
+window.showPage=(id)=>{
+document.querySelectorAll(".page").forEach(p=>p.style.display="none");
+get(id).style.display="block";
 };
 
 window.register=async()=>{
@@ -45,23 +21,19 @@ name:get("name").value,
 password:get("password").value,
 balance:0
 });
-showMsg("Account Created");
-showLogin();
+alert("Registered");
 };
 
 window.login=async()=>{
 let snap=await getDoc(doc(db,"users",get("number").value));
-if(!snap.exists()) return showMsg("User not found");
-
 let user=snap.data();
-if(user.password!==get("password").value) return showMsg("Wrong password");
+
+if(user.password!==get("password").value){
+return alert("Wrong password");
+}
 
 get("auth").style.display="none";
 get("app").style.display="block";
-
-get("usernameHome").innerText=user.name;
-get("username2").innerText=user.name;
-get("usernumber").innerText=get("number").value;
 
 localStorage.setItem("user",get("number").value);
 
@@ -70,76 +42,72 @@ loadUser();
 
 async function loadUser(){
 let snap=await getDoc(doc(db,"users",localStorage.getItem("user")));
-if(snap.exists()){
 let bal=snap.data().balance||0;
-get("walletBalance").innerText=bal;
+
+get("balance").innerText=bal;
 get("earnBalance").innerText=bal;
+
+// account bind
+let bankSnap=await getDoc(doc(db,"bank",localStorage.getItem("user")));
+if(bankSnap.exists()){
+get("bindStatus").innerText="Active";
+get("bindDot").style.background="lime";
+}else{
+get("bindStatus").innerText="Deactivate";
+get("bindDot").style.background="red";
 }
 }
 
-window.openWithdraw=()=>get("withdrawBox").classList.add("active");
-window.closeWithdraw=()=>get("withdrawBox").classList.remove("active");
-
-window.submitWithdraw=async()=>{
-let amt=Number(get("wAmount").value);
-
-let snap=await getDoc(doc(db,"users",localStorage.getItem("user")));
-let bal=snap.data().balance||0;
-
-if(bal<200) return showMsg("Minimum ₹200 required");
-if(amt>bal) return showMsg("Insufficient balance");
-
-await setDoc(doc(db,"withdraw",Date.now()+""),{
-user:localStorage.getItem("user"),
-amount:amt
-});
-
-showMsg("Withdraw Submitted");
+window.deposit=()=>{
+loadDepositSettings();
+get("depositBox").style.display="block";
 };
 
-window.deposit=()=>get("depositBox").classList.add("active");
-window.closeDeposit=()=>get("depositBox").classList.remove("active");
+window.closeDeposit=()=>{
+get("depositBox").style.display="none";
+};
+
+async function loadDepositSettings(){
+let snap=await getDoc(doc(db,"settings","deposit"));
+if(snap.exists()){
+let d=snap.data();
+get("depositAmountText").innerText="₹"+d.amount;
+get("qrImage").src=d.qr;
+get("upiText").innerText="UPI ID: "+d.upi;
+}
+}
 
 window.submitDeposit=async()=>{
 await setDoc(doc(db,"deposits",Date.now()+""),{
-user:localStorage.getItem("user"),
-utr:get("utr").value
+utr:get("utr").value,
+user:localStorage.getItem("user")
 });
-showMsg("Deposit Submitted");
+alert("Submitted");
 };
 
-window.openBank=()=>get("bankBox").classList.add("active");
-window.closeBank=()=>get("bankBox").classList.remove("active");
-
-window.saveBank=async()=>{
-await setDoc(doc(db,"bank",localStorage.getItem("user")),{
-name:get("bankName").value,
-account:get("bankAcc").value,
-ifsc:get("bankIfsc").value
-});
-showMsg("Bank Saved");
+window.openBank=()=>{
+alert("Bank popup already present in your old code");
 };
 
 window.changePassword=async()=>{
+let oldP=get("oldPass").value;
+let newP=get("newPass").value;
+let confirmP=get("confirmPass").value;
+
 let snap=await getDoc(doc(db,"users",localStorage.getItem("user")));
 let user=snap.data();
 
-if(user.password!==get("oldPass").value){
-return showMsg("Wrong old password");
+if(user.password!==oldP){
+return alert("Wrong old password");
 }
 
-if(get("newPass").value!==get("confirmPass").value){
-return showMsg("Password mismatch");
+if(newP!==confirmP){
+return alert("Mismatch");
 }
 
 await updateDoc(doc(db,"users",localStorage.getItem("user")),{
-password:get("newPass").value
+password:newP
 });
 
-showMsg("Password Changed");
-};
-
-window.logout=()=>{
-localStorage.clear();
-location.reload();
+alert("Changed");
 };
